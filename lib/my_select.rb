e1c9c1a8f6f07c73867daa246cbf7d069cd12db8
Will Saffran <willh.saffran@gmail.com>
2019-02-09 02:08:48 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,9 @@
-def my_select(collection) 
-  i =0 
-  new_array = [] 
-  while i < collection.length 
-    if (yield(array[i]))
-      new_array << collection[i] 
-    end 
-    i += 1 
-  end 
-  new_array 
-end 
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end

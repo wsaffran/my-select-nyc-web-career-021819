2641eefc20801ece0df6f4f53d1d9dc9a6cea8fb
Will Saffran <willh.saffran@gmail.com>
2019-02-09 02:10:45 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(array) 
+  i = 0
+  new_array = []
+
+  while i < array.length
+    if yield(array[i]) == true
+      new_array.push(array[i])
+    end
+    i+=1
+  end
+  new_array
 end
